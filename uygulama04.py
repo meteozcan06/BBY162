8f4b56a32a11397383ade65a505872b2df9e8e99
meteozcan06 <48053868+meteozcan06@users.noreply.github.com>
2019-04-24 14:27:24 +0300

    Add files via upload

---
+++ uygulama04.py
@@ -0,0 +1,26 @@
+﻿"""
+print(soru1)
+metin = "Açık bilim, araştırma çıktılarına ve süreçlerine herkesin serbestçe erişmesini, bunların ortak kullanımını, dağıtımını ve üretimini kolaylaştıran bilim uygulamasıdır."
+print(metin)
+ilk20karakter = metin[:21]
+print(ilk20karakter)
+"""
+"""
+print(soru2)
+liste = ["Açık Bilim", "Açık Erişim", "Açık Lisans", "Açık Eğitim", "Açık Veri", "Açık Kültür"]
+
+print(liste)
+for i in liste:
+    print(i)
+"""
+"""
+print(soru3)
+sozluk = {"Elma" : "Ağaçta yetişen bir tür meyve" , "Salatalık" : "Fidan üzerinde büyüyen bir tür sebze"}
+yazılanSozcuk = input("Sözlükte yazan bir meyve veya sebze adı giriniz. Elma/Salatalık: ")
+if yazılanSozcuk = "Elma":
+    print("Ağaçta yetişen bir tür meyve")
+if yazılanSozcuk = "Salatalık":
+    print("Fidan üzerinde büyüyen bir tür sebze")
+else
+    print("Sözlükten bir kelime giriniz.")
+"""
